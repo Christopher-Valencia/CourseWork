f711c3dec0a10fda79a730d05672647daa66b533
Chris Valencia <ckthulu@gmail.com>
2022-06-02 17:34:26 -0700

    Add files via upload

---
+++ OperatingSystems/Lab04/avg_POSIX_uni/cmake-build-debug/CMakeFiles/avg_server.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/christophervalencia/Desktop/comp362/Lab04/avg_POSIX_uni/avg_POSIX_server.c" "/home/christophervalencia/Desktop/comp362/Lab04/avg_POSIX_uni/cmake-build-debug/CMakeFiles/avg_server.dir/avg_POSIX_server.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ OperatingSystems/Lab04/avg_POSIX_uni/cmake-build-debug/CMakeFiles/avg_server.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/avg_server.dir/avg_POSIX_server.c.o"
+  "bin/avg_server"
+  "bin/avg_server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/avg_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
